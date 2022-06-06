1729767866d03255b7cbe8188eee90cfc26ea870
William Finzer <wfinzer@concord.org>
2022-06-06 07:54:01 -0700

    -- Selection, marqueeSelection, clear selection, drag selected all working properly now!

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,5 +1,5 @@
 import {graphingTypes} from "./graphing-types";
-import React, {useCallback, useEffect, useRef, useState} from "react";
+import React, {useEffect, useRef} from "react";
 import {drag, select} from "d3";
 import {clearSelection, selectCasesInWorldRect} from "../utils/data_utils";
 // import {selectCasesInWorldRect} from "../utils/data_utils";
@@ -18,50 +18,56 @@ export const Background = (props: {
 		plotX = xScale.range()[0],
 		plotY = yScale.range()[1],
 		plotWidth = xScale.range()[1] - xScale.range()[0],
-		plotHeight = yScale.range()[0] - yScale.range()[1],
-		[startX, setStartX] = useState(0),
-		[startY, setStartY] = useState(0),
-		[width, setWidth] = useState(0),
-		[height, setHeight] = useState(0),
-		onDragStart = useCallback((event: MouseEvent) => {
-			setStartX(event.x)
-			setStartY(event.y)
-			setWidth(0)
-			setHeight(0)
-			props.marquee.setRect({x: event.x, y: event.y, width: 0, height: 0})
-			props.dots.setScatterData(clearSelection(props.dots.scatterData))
-		}, [props.dots, props.marquee]),
-
-		onDrag = useCallback((event: { dx: number; dy: number; }) => {
-			if (event.dx !== 0 || event.dy !== 0) {
-				setWidth(width + event.dx)
-				setHeight(height + event.dy)
-				props.marquee.setRect({x: startX, y: startY, width, height})
-
-				const worldRect = {
-					// todo: extract translation from transform
-					x: props.dots.xScale.invert((width < 0 ? startX + width : startX) - 60),
-					y: props.dots.yScale.invert(height < 0 ? startY + height : startY),
-					width: (props.dots.xScale.invert(Math.abs(width)) - props.dots.xScale.invert(0)),
-					height: (props.dots.yScale.invert(0) - props.dots.yScale.invert(Math.abs(height)))
-				}
-
-				props.dots.setScatterData(
-					selectCasesInWorldRect(props.dots.scatterData, worldRect))
-			}
-		}, [height, props.dots, props.marquee, startX, startY, width]),
-
-		onDragEnd = useCallback(() => {
-			props.marquee.setRect({x: 0, y: 0, width: 0, height: 0})
-		}, [props.marquee])
-
+		plotHeight = yScale.range()[0] - yScale.range()[1]
 
 	useEffect(() => {
+		let startX = 0,
+			startY = 0,
+			width = 0,
+			height = 0
+		const onDragStart = (event: MouseEvent) => {
+				startX = event.x
+				startY = event.y
+				width = 0
+				height = 0
+				props.marquee.setRect({x: event.x, y: event.y, width: 0, height: 0})
+			},
+			onDrag = (event: { dx: number; dy: number; }) => {
+				if (event.dx !== 0 || event.dy !== 0) {
+					width += event.dx
+					height += event.dy
+					props.marquee.setRect(prevRect => {
+						return {
+							x: prevRect.x, y: prevRect.y,
+							width: prevRect.width + event.dx,
+							height: prevRect.height + event.dy
+						}
+					})
+
+					const worldRect = {
+						// todo: extract translation from transform
+						x: props.dots.xScale.invert((width + event.dx < 0 ? startX + width + event.dx : startX) - 60),
+						y: props.dots.yScale.invert(height + event.dy < 0 ? startY + height + event.dy : startY),
+						width: (props.dots.xScale.invert(Math.abs(width + event.dx)) - props.dots.xScale.invert(0)),
+						height: (props.dots.yScale.invert(0) - props.dots.yScale.invert(Math.abs(height + event.dy)))
+					}
+
+					props.dots.setScatterData( (prevData)=>
+						selectCasesInWorldRect(prevData, worldRect))
+				}
+			},
+			onDragEnd = () => {
+				props.marquee.setRect({x: 0, y: 0, width: 0, height: 0})
+			}
+
 		const dragBehavior = drag()
 				.on("start", onDragStart)
 				.on("drag", onDrag)
 				.on("end", onDragEnd),
 			groupElement = ref.current
+			select(groupElement).on('click',()=>{
+				props.dots.setScatterData(clearSelection(props.dots.scatterData))
+			})
 
 		select(groupElement)
 			.selectAll('rect')
@@ -84,8 +90,7 @@ export const Background = (props: {
 						.attr('height', plotHeight)
 				}
 			)
-	}, [props.dots.transform, props.dots, props.marquee, xScale, yScale, plotX, plotY, plotWidth, plotHeight,
-		height, startX, startY, width, onDragStart, onDrag, onDragEnd])
+	}, [props.dots.transform, props.dots, props.marquee, xScale, yScale, plotX, plotY, plotWidth, plotHeight])
 
 	return (
 		<g>
